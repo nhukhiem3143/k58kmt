471865122529b291efa4d3e9f7266aa67b563294
Nhu Khiem <162820571+nhukhiem3143@users.noreply.github.com>
2025-03-29 13:51:30 +0700

    Add files via upload

---
+++ truy_xuat.sql
@@ -0,0 +1,17 @@
+﻿WITH DiemThanhPhan AS (
+    SELECT 
+        dkmh.MaSV,
+        ROUND(AVG(d.diem), 3) AS Diem_TP,  -- Làm tròn đến 3 chữ số thập phân
+        COUNT(*) AS SoLuongDiem  -- Đếm số lượng điểm thành phần
+    FROM Diem d 
+    JOIN DKMH dkmh ON d.id_dk = dkmh.id_dk
+    GROUP BY dkmh.MaSV
+)
+SELECT 
+    dkmh.id_dk, dkmh.MaSV, sv.HoTen,sv.NgaySinh, 
+    dkmh.MaLopHP,dtp.Diem_TP, 
+    dtp.SoLuongDiem, dkmh.DiemThi, dkmh.PhanTramThi,
+    ROUND((dtp.Diem_TP * 0.4 + dkmh.DiemThi * dkmh.PhanTramThi), 3) AS DiemTong  -- Làm tròn đến 3 chữ số thập phân
+FROM DKMH dkmh
+JOIN SinhVien sv ON dkmh.MaSV = sv.MaSV
+JOIN DiemThanhPhan dtp ON dkmh.MaSV = dtp.MaSV;
