d9bfdf9f019127bf6136ec0905b9cfa2bb54325f
Nhu Khiem <162820571+nhukhiem3143@users.noreply.github.com>
2025-03-29 23:10:05 +0700

    Update truy_xuat.sql

--- truy_xuat.sql
+++ truy_xuat.sql
@@ -9,10 +9,10 @@ WITH DiemThanhPhan AS (
     GROUP BY dkmh.id_dk, dkmh.MaSV  
 )
 SELECT 
-    dkmh.id_dk, dkmh.MaSV, sv.HoTen, sv.NgaySinh, 
-    dkmh.MaLopHP, dtp.Diem_TP, 
-    dtp.SoLuongDiem, dkmh.DiemThi, dkmh.PhanTramThi,
-    ROUND((dtp.Diem_TP * (1 - dkmh.PhanTramThi) + dkmh.DiemThi * dkmh.PhanTramThi), 3) AS DiemTong  -- Tính tổng điểm
+    dkmh.id_dk [ID đăng ký], dkmh.MaSV [Mã Sinh Viên], sv.HoTen [Họ và tên], sv.NgaySinh [Ngày sinh],
+    dkmh.MaLopHP [Mã lớp HP], dtp.Diem_TP [Điểm thành phần], 
+    dtp.SoLuongDiem [Số lượng điểm], dkmh.DiemThi [Điểm thi], dkmh.PhanTramThi [Phần trăm thi],
+    ROUND((dtp.Diem_TP * (1 - dkmh.PhanTramThi) + dkmh.DiemThi * dkmh.PhanTramThi), 3) AS [Điểm Tổng]  -- Tính tổng điểm
 FROM DKMH dkmh
 JOIN SinhVien sv ON dkmh.MaSV = sv.MaSV
 JOIN DiemThanhPhan dtp ON dkmh.id_dk = dtp.id_dk; 
