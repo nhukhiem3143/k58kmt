43814d75983155781223eff9d5228d2023a0fcdf
Nhu Khiem <162820571+nhukhiem3143@users.noreply.github.com>
2025-03-29 16:52:43 +0700

    truy_xuat.sql

--- truy_xuat.sql
+++ truy_xuat.sql
@@ -11,7 +11,7 @@ SELECT
     dkmh.id_dk, dkmh.MaSV, sv.HoTen,sv.NgaySinh, 
     dkmh.MaLopHP,dtp.Diem_TP, 
     dtp.SoLuongDiem, dkmh.DiemThi, dkmh.PhanTramThi,
-    ROUND((dtp.Diem_TP * 0.4 + dkmh.DiemThi * dkmh.PhanTramThi), 3) AS DiemTong  -- Làm tròn đến 3 chữ số thập phân
+    ROUND((dtp.Diem_TP * (1 - dkmh.PhanTramThi) + dkmh.DiemThi * dkmh.PhanTramThi), 3) AS DiemTong  -- Làm tròn đến 3 chữ số thập phân
 FROM DKMH dkmh
 JOIN SinhVien sv ON dkmh.MaSV = sv.MaSV
 JOIN DiemThanhPhan dtp ON dkmh.MaSV = dtp.MaSV;
